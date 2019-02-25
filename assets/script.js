d21249999ed46014627fb3af88854d3e144f7e07
MoisesAlVe <moalve7690@outlook.com>
2019-02-25 17:09:02 -0600

    basic structure, start

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+//formulario
+const variedadForm = document.querySelector('#orden-form');
+//entradas
+const variedad = document.querySelector('#variedad');
+const cantidad = document.querySelector('#cantidad');
+const picante = document.querySelector('#picante')
+//secciones
+const lista = document.querySelector('#lista');
+
+variedadForm.addEventListener('submit', function(){
+  event.preventDefault();
+  console.log('detuvimos el submit');
+  const variedadElegida = variedad.value;
+  const cantidadPedida = cantidad.value;
+  const agregarPicante = picante.value;
+
+  let imagenTorta;
+  if (variedadElegida == 'chilanga') {
+    imagenTorta = 'torta-chilanga.jpg';
+  } else {
+    imagenTorta = 'torta-cubana.jpg';
+  }
+
+  const comanda = `
+      <li class="nueva-comanda">
+        <h3>Esta es tu orden:</h3>
+        <p>${cantidadPedida} ${variedadElegida} con ${agregarPicante} de picante.</p>
+        <img src='assets/img/${imagenTorta}'>
+      </li>
+      `
+  lista.innerHTML += comanda;
+});
+
+//`https://wa.me/5215513820832?text=Hola%20quiero%20ordenar%20${comanda}`
+//Jake: Chorizo, piña y milanesa
+//Galilea: Milanesa, pechuga y pierna
+//Maribel: Pierna, jamón, piña y quesillo
+//Lucero: Pollo, queso panela y quesillo
+//Lalo-k: Pechuga, pierna y salchicha
